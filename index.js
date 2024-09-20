d5f54baeb977eaf53ce6a03345ae5e9f0831ac7c
TriUbi <triubilla@gmail.com>
2024-09-20 09:32:32 +0200

    oficiell kod

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function updatePrice() {
     let employeeValues = [0, 1, 25, 50, 70];
     let selectedEmployeeValue = employeeValues[sliderValue];
 
-    let basePrice = 1900; 
+    let basePrice = 1900; // Pris per anställd
 
     let totalPrice = basePrice * selectedEmployeeValue;
     
@@ -20,29 +20,32 @@ function updatePrice() {
         priceElement.textContent = totalPrice.toLocaleString();
         perMonth.style.display = "inline";
     }
-     // Uppdaterar 1+ label position
-     let antalMedarbetare = document.querySelector('.antalMedarbetare');
-     let sliderWidth = employeeSlider.offsetWidth;
-     let max = employeeSlider.max;
-     let min = employeeSlider.min;
- 
-     
-     let position = ((sliderValue - min) / (max - min)) * (sliderWidth - 40); 
-     antalMedarbetare.style.left = `${position}px`; 
- 
-        // Uppdaterar värdet av antal medarbetare baserat på slidern value
-        
-          if (selectedEmployeeValue === 1) {
-         antalMedarbetare.querySelector('span').textContent = `${selectedEmployeeValue}+`;
-     } else {
-         antalMedarbetare.querySelector('span').textContent = `${selectedEmployeeValue}`;
-     }
- }
- 
- window.onload = function() {
-     updatePrice();
- };
- 
- window.onresize = function() {
-     updatePrice();
- }
+
+    // Uppdaterar 1+ label position
+    let antalMedarbetare = document.querySelector('.antalMedarbetare');
+    let sliderWidth = employeeSlider.offsetWidth;
+    let max = employeeSlider.max;
+    let min = employeeSlider.min;
+
+    
+    let position = ((sliderValue - min) / (max - min)) * (sliderWidth - 40); 
+    antalMedarbetare.style.left = `${position}px`;
+
+
+       // Uppdaterar värdet av antal medarbetare baserat på slidern value
+       
+       if (selectedEmployeeValue === 1) {
+        antalMedarbetare.querySelector('span').textContent = `${selectedEmployeeValue}+`;
+    } else {
+        antalMedarbetare.querySelector('span').textContent = `${selectedEmployeeValue}`;
+    }    
+
+window.onload = function() {
+    updatePrice();
+};
+
+window.onresize = function() {
+    updatePrice();
+}
+
+}
